15f4e1c3fd73971ee8b33216e4bc9012def58228
batuhan <batuhanonur29@gmail.com>
2020-06-01 16:40:29 +0300

    quiz app

---
+++ quiz-app/src/views/Level2.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Level2 extends Component {
+    render() {
+        return (
+            <div>
+                Level2
+            </div>
+        )
+    }
+}
+
+export default Level2;
